2eb0e12f0d4b39a1f991432a85f268d3ed30f3ca
belo355 <edilson.belo@live.com>
2020-12-14 00:03:21 -0300

    add test units for controllers

---
+++ src/test/java/br/com/hackaton/zup/bank/BankApplicationTest.java
@@ -0,0 +1,18 @@
+package br.com.hackaton.zup.bank;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankApplicationTest {
+
+    @Test
+    public void test()
+    {
+        Assert.assertTrue(true);
+    }
+}
