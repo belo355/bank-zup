322bd210d72a83e8e5b765ba7140da1e83fc1e2f
belo355 <edilson.belo@live.com>
2021-03-06 23:06:40 -0300

    confg spring admin

--- src/main/java/br/com/hackaton/zup/bank/controller/ProposalController.java
+++ src/main/java/br/com/hackaton/zup/bank/controller/ProposalController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import br.com.hackaton.zup.bank.controller.form.AccountProposalForm;
 import br.com.hackaton.zup.bank.model.Proposal;
 import br.com.hackaton.zup.bank.repository.ProposalRepository;
-import org.slf4j.Logge
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
